7a499058229e9ae50e74fd81481835ca2e23c308
TheSchlote <TheSchlote@gmail.com>
2024-12-26 22:13:20 -0600

    Controls
    3

--- Player.cs
+++ Player.cs
@@ -3,10 +3,22 @@ using System;
 
 public partial class Player : Node
 {
-    private Vector2 _moveDirection = Vector2.Zero;
+    [Export]
+    public CharacterBody3D Character { get; set; }
+    [Export]
+    public Camera3D Camera { get; set; }
+    private Vector2 _inputDirection = Vector2.Zero;
+    private Vector3 _moveDirection = Vector3.Zero;
     public override void _Process(double delta)
     {
-        _moveDirection = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
-        GD.Print(_moveDirection);
+        // Get input direction
+        _inputDirection = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBackward");
+
+        // Calculate movement direction based on camera orientation
+        _moveDirection = (Camera.Basis.X * new Vector3(1, 0, 1).Normalized()) * _inputDirection.X;
+        _moveDirection += (Camera.Basis.Z * new Vector3(1, 0, 1).Normalized()) * _inputDirection.Y;
+
+        // Move character
+        Character.MoveAndCollide(_moveDirection * (float)delta);
     }
 }
